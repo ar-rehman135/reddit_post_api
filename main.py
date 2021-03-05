ce1f5effa57cc3191d31259926b6476110a15c0d
ar-rehman135 <abdulrehmanmirza135@gmail.com>
2021-03-05 20:45:07 +0500

    erro_handle

--- main.py
+++ main.py
@@ -13,11 +13,6 @@ def get_ticker():
     if 'ticker' in request.args:
         ticker = request.args.get('ticker')
         ticker = str(ticker).upper()
-        get_ticker_data_from_scores = Scores.query.filter(Scores.stock_ticker == ticker).first()
-        if not get_ticker_data_from_scores:
-            score = ''
-            mention = ''
-        ######### Posts data #####################################
         get_ticker_data_from_post = Posts.query.filter(Posts.stock_ticker == ticker).first()
         if not get_ticker_data_from_post:
             err = {
@@ -25,34 +20,46 @@ def get_ticker():
                 "code": "201"
             }
             return json.dumps(err)
-        score = get_ticker_data_from_scores.score
-        mention = get_ticker_data_from_scores.mention
-        logo = get_ticker_data_from_post.logo
-        industry = get_ticker_data_from_post.industry
-        sector = get_ticker_data_from_post.sector
-        market_cap = get_ticker_data_from_post.market_cap
-        employees = get_ticker_data_from_post.employees
-        url = get_ticker_data_from_post.url
-        description = get_ticker_data_from_post.description
-        company_name = get_ticker_data_from_post.company_name
-        similiar_companies = get_ticker_data_from_post.similiar_companies
+        get_ticker_data_from_scores = Scores.query.filter(Scores.stock_ticker == ticker).first()
+        if not get_ticker_data_from_scores:
+            score = ''
+            mention = ''
 
-        data = {
-            "score": score,
-            "mention": mention,
-            "logo": logo,
-            "sector": sector,
-            "market_cap": market_cap,
-            "employees": employees,
-            "url": url,
-            "description": description,
-            "company_name": company_name,
-            "similiar_companies": similiar_companies,
-            "industry": industry
+        if get_ticker_data_from_scores:
+            score = get_ticker_data_from_scores.score
+            mention = get_ticker_data_from_scores.mention
+        if get_ticker_data_from_post:
+            logo = get_ticker_data_from_post.logo
+            industry = get_ticker_data_from_post.industry
+            sector = get_ticker_data_from_post.sector
+            market_cap = get_ticker_data_from_post.market_cap
+            employees = get_ticker_data_from_post.employees
+            url = get_ticker_data_from_post.url
+            description = get_ticker_data_from_post.description
+            company_name = get_ticker_data_from_post.company_name
+            similiar_companies = get_ticker_data_from_post.similiar_companies
+
+            data = {
+                "score": score,
+                "mention": mention,
+                "logo": logo,
+                "sector": sector,
+                "market_cap": market_cap,
+                "employees": employees,
+                "url": url,
+                "description": description,
+                "company_name": company_name,
+                "similiar_companies": similiar_companies,
+                "industry": industry
+            }
+
+            d = json.dumps(data)
+            return d
+        err = {
+            "message": "Invalid Ticker " + ticker,
+            "code": "201"
         }
-
-        d = json.dumps(data)
-        return d
+        return json.dumps(err)
     else:
         err = {
             "message": "Invalid Data",
